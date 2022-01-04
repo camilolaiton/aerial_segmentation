89bbbc21686ac2908e01aa587fc5eb585dcb5a0c
Camilo Laiton <kmilo9713.cl@gmail.com>
2022-01-04 15:14:10 -0500

    new loss function

--- Pytorch/model/losses.py
+++ Pytorch/model/losses.py
@@ -92,35 +92,47 @@ class FocalDiceLoss(nn.Module):
         return loss
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, alpha=None, size_average=True):
+    """FocalLoss.
+
+    .. seealso::
+        Lin, Tsung-Yi, et al. "Focal loss for dense object detection."
+        Proceedings of the IEEE international conference on computer vision. 2017.
+
+    Args:
+        gamma (float): Value from 0 to 5, Control between easy background and hard ROI
+            training examples. If set to 0, equivalent to cross-entropy.
+        alpha (float): Value from 0 to 1, usually corresponding to the inverse of class frequency to address class
+            imbalance.
+        eps (float): Epsilon to avoid division by zero.
+
+    Attributes:
+        gamma (float): Value from 0 to 5, Control between easy background and hard ROI
+            training examples. If set to 0, equivalent to cross-entropy.
+        alpha (float): Value from 0 to 1, usually corresponding to the inverse of class frequency to address class
+            imbalance.
+        eps (float): Epsilon to avoid division by zero.
+    """
+
+    def __init__(self, gamma=2, alpha=0.25, eps=1e-7):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
+        self.eps = eps
 
     def forward(self, input, target):
-        if input.dim()>2:
-            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
+        input = input.clamp(self.eps, 1. - self.eps)
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1,target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
+        cross_entropy = - (target * torch.log(input) + (1 - target) * torch.log(1 - input))  # eq1
+        logpt = - cross_entropy
+        pt = torch.exp(logpt)  # eq2
 
-        if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
+        at = self.alpha * target + (1 - self.alpha) * (1 - target)
+        balanced_cross_entropy = - at * logpt  # eq3
 
-        loss = -1 * (1-pt)**self.gamma * logpt
-        if self.size_average: return loss.mean()
-        else: return loss.sum()
+        focal_loss = balanced_cross_entropy * ((1 - pt) ** self.gamma)  # eq5
+
+        return focal_loss.sum()
+
 
 def ce_loss(true, logits, weights, ignore=255):
     """Computes the weighted multi-class cross-entropy loss.
