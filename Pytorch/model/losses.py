f6f97147272ff8bc57d40d8c7622e5eab7c3620c
Camilo Laiton <kmilo9713.cl@gmail.com>
2022-01-04 15:51:11 -0500

    focal dice loss

--- Pytorch/model/losses.py
+++ Pytorch/model/losses.py
@@ -76,7 +76,7 @@ class FocalDiceLoss(nn.Module):
         self.dice = DiceLoss()
 
     def forward(self, input, target, weights=None):
-        dc_loss = - self.dice(input, target, weights)
+        dc_loss = self.dice(input, target, weights)
         fc_loss = self.focal(input, target)
 
         # used to fine tune beta
