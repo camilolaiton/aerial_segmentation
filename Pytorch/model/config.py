24701e3d12eb1ce9e9a9c57b99fa05499c26b4a8
Camilo Laiton <kmilo9713.cl@gmail.com>
2022-01-04 09:28:00 -0500

    changing transformer params

--- Pytorch/model/config.py
+++ Pytorch/model/config.py
@@ -30,7 +30,7 @@ def get_config():
             'proj_kernel':3,
             'kv_proj_stride':2,
             'depth':2,
-            'heads':4,
+            'heads':2,
             'mlp_mult':4,
             'dropout':0.1
         },
@@ -38,7 +38,7 @@ def get_config():
             'dim': 32,
             'proj_kernel':3,
             'kv_proj_stride':2,
-            'depth':4,
+            'depth':6,
             'heads':4,
             'mlp_mult':4,
             'dropout':0.1
